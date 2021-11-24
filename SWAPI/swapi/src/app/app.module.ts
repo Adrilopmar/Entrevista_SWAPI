6e1e5e914c4567feaedc67dd99cb594b1f10ebe1
Adrià <adria.lop.mar@gmail.com>
2021-11-25 00:59:44 +0100

    updated files

--- SWAPI/swapi/src/app/app.module.ts
+++ SWAPI/swapi/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
+import { StarshipsComponent } from './starships/starships.component';
+import{InfiniteScrollModule} from 'ngx-infinite-scroll'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    HeaderComponent
+    HeaderComponent,
+    StarshipsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InfiniteScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
