64d34c349511365da53fbe82574058c85bf6b724
Adrià <adria.lop.mar@gmail.com>
2021-12-02 09:59:10 +0100

    updating CORS

--- SWAPI/swapi/src/app/services/request.service.ts
+++ SWAPI/swapi/src/app/services/request.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +8,16 @@ import {HttpClient} from '@angular/common/http'
 export class RequestService {
 
   constructor(private http:HttpClient) { }
-  api_url:string = 'https://swapi.dev/api/starships/'
+  api_url:string = '/api/starships/'
   endPoint:string='?page='
   p:number=1
 
-getStarships(p:number){
-  return this.http.get(this.api_url+this.endPoint+ p)
+getStarships(p:number):Observable<any>{
+  const headers:any = new Headers();
+  headers.append('Access-Control-Allow-Headers', '*');
+        headers.append('Access-Control-Allow-Methods', 'GET');
+        headers.append('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept');
+  return this.http.get(this.api_url+this.endPoint+ p, {headers:headers})
 }
 
 }
